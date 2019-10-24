54dc2991307d13076c7518f3910ae889af3df6f8
Luis Vega <vegaluisjose@gmail.com>
2019-10-24 12:11:01 -0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod lib;
 
+#[allow(unused_imports)]
+#[allow(dead_code)]
 fn main() {
+    use std::rc::Rc;
     use lib::*;
     use Info::*;
     use Type::*;
@@ -11,9 +14,5 @@ fn main() {
     use Stmt::*;
     use DefModule::*;
     use DefCircuit::*;
-    let name = "top";
-    let port = vec![Port(NoInfo, "i0".into(), Input, UInt(32)), Port(NoInfo, "i1".into(), Input, UInt(32))];
-    let module = Module(NoInfo, name.into(), port, EmptyStmt);
-    let cir = Circuit(NoInfo, vec![module], name.into());
-    emit(cir, "two");
+    println!("Main");
 }
